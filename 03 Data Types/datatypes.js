dd28aa8b336ec9d004dd3fa1d300d6cf9627fd51
K Subramanyeshwara <subramanyeshwarak@gmail.com>
2023-06-14 23:34:31 +0530

    Data Types code files added

---
+++ 03 Data Types/datatypes.js
@@ -0,0 +1,8 @@
+//Primitive data types are immutable
+
+let primitive = 'immutable';
+console.log( primitive );
+primitive[0] ='m';
+console.log( primitive );
+primitive = 'Primitive'
+console.log( primitive );
